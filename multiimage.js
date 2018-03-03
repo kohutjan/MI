259019857ac08716072d952dbb87b824f7f38049
Jan Kohut <kohuthonza@gmail.com>
2018-03-03 14:52:08 +0100

    Circle area.

--- multiimage.js
+++ multiimage.js
@@ -1,14 +1,26 @@
-/*
+var polygonDrawn = false;
+
 function getPixelsFromFloat(value)
 {
   return Math.floor(value).toString().concat("px");
 }
 
+
+function hideCircle(circle)
+{
+  circle.style.display = 'none';
+}
+
+
 function hidePolygon(multiimage)
 {
-  for (var i = 1; i < multiimage.children.length; ++i)
+  if (polygonDrawn)
   {
-    multiimage.children[i].style.display = 'none';
+    for (var i = 1; i < multiimage.children.length; ++i)
+    {
+      multiimage.children[i].style.display = 'none';
+    }
+    polygonDrawn = false;
   }
 }
 
@@ -22,44 +34,86 @@ function drawPolygonImage(image, topOffset, leftOffset)
 }
 
 
-function drawPolygon(multiimage, r)
+function clickThroughCircle(circle)
 {
-  console.log("lol");
-  var firstImage = multiimage.firstElementChild.firstElementChild;
-  var firstImagePosition = firstImage.getBoundingClientRect();
-  var centerTop = firstImagePosition.top;
-  var centerLeft = firstImagePosition.left;
-  var polygonImagesLength = multiimage.children.length - 1;
-  var alfa = 2 * Math.PI / polygonImagesLength;
-  for (var i = 0; i < polygonImagesLength; ++i)
+  var scrollTop  = window.pageYOffset || document.documentElement.scrollTop;
+  var scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+  circle.style.display = 'none';
+  document.elementFromPoint(event.clientX , event.clientY).click();
+  circle.style.display = '';
+}
+
+
+function drawCircle(multiimage, centerTop, centerLeft, r, polygonImageSize)
+{
+  var circleDiv = multiimage.getElementsByTagName('div');
+  if (typeof circleDiv !== 'undefined' && circleDiv.length > 0)
   {
-    var actualAlfa = i * alfa;
-    var relativeTopOffset = r * Math.sin(actualAlfa);
-    var relativeLeftOffset = r * Math.cos(actualAlfa);
-    var topOffset = centerTop - relativeTopOffset;
-    var leftOffset = centerLeft - relativeLeftOffset;
-    drawPolygonImage(multiimage.children[i + 1], topOffset, leftOffset);
+    var circle = circleDiv[0];
+  }
+  else
+  {
+    var circle = document.createElement("div");
+    circle.addEventListener("click", function (){ clickThroughCircle(circle); });
+    circle.addEventListener("mouseleave", function () { setTimeout(function ()
+                                                        { hideCircle(circle); }, 1200);
+                                                        setTimeout(function ()
+                                                        { hidePolygon(multiimage); }, 1200)});
+    multiimage.appendChild(circle);
+  }
+  circle.style.display = '';
+  circle.style.position = 'absolute';
+  circle.style.zIndex = '1';
+  circle.style.cursor = 'pointer';
+  circle.style.top = getPixelsFromFloat(centerTop - r);
+  circle.style.left = getPixelsFromFloat(centerLeft - r);
+  circle.style.width = getPixelsFromFloat(2 * r + polygonImageSize);
+  circle.style.height = circle.style.width;
+  circle.style.borderRadius = getPixelsFromFloat(r + polygonImageSize);
+}
+
+
+function drawPolygon()
+{
+  if (!polygonDrawn)
+  {
+    var r = 250;
+    var multiimage = this;
+    var firstImage = multiimage.firstElementChild.firstElementChild;
+    var firstImagePosition = firstImage.getBoundingClientRect();
+    var scrollTop  = window.pageYOffset || document.documentElement.scrollTop;
+    var scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+    var centerTop = firstImagePosition.top + scrollTop;
+    var centerLeft = firstImagePosition.left + scrollLeft;
+    var polygonImageSize = firstImagePosition.right - firstImagePosition.left;
+    drawCircle(multiimage, centerTop, centerLeft, r, polygonImageSize);
+    var polygonImagesLength = multiimage.children.length - 2;
+    var alfa = 2 * Math.PI / polygonImagesLength;
+    for (var i = 0; i < polygonImagesLength; ++i)
+    {
+      var actualAlfa = i * alfa;
+      var relativeTopOffset = r * Math.sin(actualAlfa);
+      var relativeLeftOffset = r * Math.cos(actualAlfa);
+      var topOffset = centerTop - relativeTopOffset;
+      var leftOffset = centerLeft - relativeLeftOffset;
+      drawPolygonImage(multiimage.children[i + 1], topOffset, leftOffset);
+    }
+    polygonDrawn = true;
+    console.log(polygonDrawn);
   }
 }
 
+
 function addListeners(multiimage)
 {
-  multiimage.addEventListener("mouseover", drawPolygon(multiimage, 250), false);
-  multiimage.addEventListener("mouseout", hidePolygon(multiimage), false);
+  multiimage.addEventListener("mouseover", drawPolygon);
 }
 
-*/
-function testim() {
+function blackCircle()
+{
   this.style.background = 'black';
-};
+}
 
-function testim2() {
-  this.style.background = 'white';
-};
 
-//var multiimage = Array.from(document.getElementsByClassName('multiimage'));
-var test = document.getElementsByClassName('test')[0];
-test.addEventListener("mouseenter", testim);
-test.addEventListener("mouseleave", testim2);
-
-//multiimage[0].firstElementChild.firstElementChild.addEventListener("mouseout", hidePolygon(multiimage[0]), false);
+var multiimage = Array.from(document.getElementsByClassName('multiimage'));
+multiimage.forEach(addListeners);
