adaac10ee349c5dcdaa585df3ddf189439193b24
Jan Kohut <kohuthonza@gmail.com>
2018-03-04 16:57:35 +0100

    Show image.

--- multiimage.js
+++ multiimage.js
@@ -87,10 +87,74 @@ function drawPolygonImage(image, topOffset, leftOffset)
 }
 
 
-function clickThroughCircle(circle)
+function hideShowImage(multiimage, imageWrapper)
+{
+  multiimage['element'].removeChild(imageWrapper);
+  multiimage['imageShown'] = false;
+  clearTimeout(multiimage['timerOutsideCircle']);
+  multiimage['timerOutsideCircle'] = setTimeout(function ()
+                                     { animateHidePolygon(multiimage, 250, 10, 150, 250); }, 1200);
+}
+
+
+function drawShowImage(multiimage, imageWrapper, imageToShow, closeButton)
+{
+  imageWrapper.style.position = 'fixed';
+  imageWrapper.style.top = '0';
+  imageWrapper.style.bottom = '0';
+  imageWrapper.style.left = '0';
+  imageWrapper.style.right = '0';
+  imageWrapper.style.maxWidth = '100%';
+  imageWrapper.style.maxHeight = '100%';
+  imageWrapper.style.background = 'black';
+  imageWrapper.style.zIndex = '100';
+  imageToShow.style.position = 'fixed';
+  imageToShow.style.top = '0';
+  imageToShow.style.bottom = '0';
+  imageToShow.style.left = '0';
+  imageToShow.style.right = '0';
+  imageToShow.style.maxWidth = '100%';
+  imageToShow.style.maxHeight = '100%';
+  imageToShow.style.margin = 'auto';
+  imageToShow.style.overflow = 'auto';
+  imageToShow.style.zIndex = '101';
+  closeButton.style.position = 'absolute';
+  closeButton.style.right = '0px';
+  closeButton.style.zIndex = '102';
+  closeButton.style.backgroundRepeat = 'no-repeat';
+  closeButton.style.background = 'rgba(219,221,224,0.4)';
+  closeButton.style.border = 'none';
+  closeButton.style.borderRadius = '30px';
+  closeButton.style.cursor = 'pointer';
+  closeButton.style.overflow = 'hidden';
+  closeButton.style.outline = 'none';
+  closeButton.style.color = 'white';
+  closeButton.style.height = '50px';
+  closeButton.style.width = '50px';
+  closeButton.style.fontSize = '40px';
+  closeButton.style.margin = '10px';
+  closeButton.addEventListener('mouseenter', function() { closeButton.style.color = 'black'});
+  closeButton.addEventListener('mouseleave', function() { closeButton.style.color = 'white'});
+  multiimage['imageShown'] = true;
+}
+
+
+function clickThroughCircle(multiimage, circle)
 {
   circle.style.display = 'none';
-  document.elementFromPoint(event.clientX , event.clientY).click();
+  var image = document.elementFromPoint(event.clientX , event.clientY).parentElement;
+  var imageWrapper = document.createElement('DIV');
+  var imageToShow = document.createElement('IMG');
+  var closeButton = document.createElement('BUTTON');
+  var cross = document.createTextNode("X");
+  imageToShow.setAttribute('src', image.getAttribute('href'));
+  multiimage['element'].appendChild(imageWrapper);
+  imageWrapper.appendChild(imageToShow);
+  imageWrapper.appendChild(closeButton);
+  closeButton.appendChild(cross);
+  closeButton.addEventListener('click', function () { hideShowImage(multiimage, imageWrapper) })
+  drawShowImage(multiimage, imageWrapper, imageToShow, closeButton);
+  console.log(imageToShow);
   circle.style.display = '';
 }
 
@@ -105,9 +169,9 @@ function drawCircle(multiimage, centerTop, centerLeft, r, polygonImageSize)
   {
     var circle = document.createElement('div');
     multiimage['element'].appendChild(circle);
-    circle.addEventListener('click', function (){ clickThroughCircle(circle); });
+    circle.addEventListener('click', function (){ clickThroughCircle(multiimage, circle); });
     circle.addEventListener('mouseleave', function () { multiimage['mouseInsideCircle'] = false;
-                                                        setTimeout(function ()
+                                                        multiimage['timerOutsideCircle'] = setTimeout(function ()
                                                       { animateHidePolygon(multiimage, 250, 10, 150, 250); }, 1200)});
     circle.addEventListener('mouseenter', function () { multiimage['mouseInsideCircle'] = true; });
   }
@@ -149,7 +213,7 @@ function drawPolygon(multiimage, r)
 
 function animateHidePolygon(multiimage, r, step, stopR, actualR)
 {
-  if (!multiimage['mouseInsideCircle'] && multiimage['polygonDrawn'])
+  if (!multiimage['mouseInsideCircle'] && multiimage['polygonDrawn'] && !multiimage['imageShown'])
   {
     setTimeout(function ()
     {
@@ -208,7 +272,9 @@ function initMultiimages()
     multiimages.push({element: multiimagesElements[i],
                       polygonDrawn: false,
                       mouseInsideCircle: false,
-                      mouseInTheMiddle: false});
+                      timerOutsideCircle: i,
+                      mouseInTheMiddle: false,
+                      imageShown: false});
   }
 }
 
